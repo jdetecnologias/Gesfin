8b5603321f50dc2c06dc3960326c3585a6147c6c
jdtecnologias <jdtecnologias@gmail.com>
2019-02-01 10:05:56 -0200

    branch req0007 - Layout do componente Seletor Mes corrigdo

--- js/seletorMes.js
+++ js/seletorMes.js
@@ -3,14 +3,14 @@ seletor.setTemplate(function(mes = 0,ano = 2019){
 	var meses = getMeses();
 	return `
 		<div id="seletorMes" class="limparFloat center" mes="${mes}" ano="${ano}">
-			<button class="dec flutuarEsq">
-				<
+			<button class="primaria bto dec flutuarEsq">
+				<i class="fas fa-arrow-alt-circle-left"></i>
 			</button>
 			<div class="month flutuarEsq center">
 				${meses[mes]} - ${ano}
 			</div>
-			<button class="inc flutuarEsq center">
-				>
+			<button class="bto primaria inc flutuarEsq center">
+				<i class="fas fa-arrow-alt-circle-right"></i>
 			</button>
 		
 		</div>
@@ -47,46 +47,41 @@ seletor.defCss(function(){
 				width:100%;
 				height:50px;
 			}
-			
-			.month{
-				line-height:50px;
-				width:80%;
-				height:50px;
-			}
-			.dec {
-				line-height:50px;
-				width:10%;
-				height:50px;
-			}
-			.inc{
-				line-height:50px;
-				width:10%;
-				height:50px;
-			}
 	}
 		@media screen and (min-width:641px){
 			#seletorMes{
-				width:30%;
-				
+				width:40%;	
+			}			
+	}
+			#seletorMes{
+				margin:5px 0 5px 0;
+				height:51px;
+			}	
+			
+			#seletorMes i{
+				font-size:26px;
+				line-height:40px;
 			}
-						
 			.month{
+				background-color:rgba(255,165,0,1);
+				font-family:calibri, sans-serif, arial;		
+				font-weight:800;
+				font-size:22px;
 				line-height:50px;
 				width:80%;
 				height:50px;
 			}
 			.dec {
-				line-height:50px;
+				cursor:pointer;
 				width:10%;
 				height:50px;
 			}
 			.inc{
-				line-height:50px;
+				cursor:pointer;
 				width:10%;
 				height:50px;
 			}
-			
-	}
+
 	`;
 
 });
